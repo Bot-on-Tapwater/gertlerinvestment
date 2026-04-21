020dd2bd0990e038842a7331de12ecd35ecc8b91
Bot-on-Tapwater <mundabrandon@outlook.com>
2026-04-21 21:13:54 +0300

    chore: Finish implementing hero section for desktop

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,7 +6,7 @@ import { motion } from "motion/react";
 function PicHeroSection() {
   return (
     <>
-      <div className="relative">
+      <div className="relative col-span-1 flex items-start">
         <Image
           alt="Picture of houses"
           width={2000}
@@ -14,7 +14,6 @@ function PicHeroSection() {
           src="/images/home/houses.jpg"
           className="w-lg rounded-2xl"
         />
-        {/* <SmallContainerPicHeroSection /> */}
       </div>
     </>
   );
@@ -23,8 +22,26 @@ function PicHeroSection() {
 function HeroSection() {
   return (
     <>
-      <section>
+      <section className="grid grid-cols-2 space-x-32 justify-self-center">
         <PicHeroSection />
+        <section className="col-span-1 flex flex-col gap-y-8">
+          <section className="bg-summer/15 text-summer font-extrabold rounded-2xl flex items-center justify-center w-sm">
+            <div className="p-2">Handpicked, verified real estate listings</div>
+          </section>
+          <div className="text-summerfive font-extrabold text-6xl">
+            Your trusted source <br />
+            for safe listings.
+          </div>
+          <div className="h-12"></div>
+          <div className="text-summerfive font-extralight text-lg w-sm">
+            At Gertler Investment, every property listing is carefully verified
+            and handpicked by our team. We’re committed to providing a safe
+            experience for every homebuyer.
+          </div>
+          <button className="bg-summer w-48 p-4 rounded-2xl text-summertwo font-semibold cursor-pointer hover:bg-summertwo hover:border-1 hover:border-summer hover:text-summer duration-200">
+            Browse listings
+          </button>
+        </section>
       </section>
     </>
   );
@@ -33,7 +50,7 @@ function HeroSection() {
 export default function HomePage() {
   return (
     <>
-      <section className="flex flex-col gap-y-16 items-center w-full">
+      <section className="flex flex-col gap-y-16 items-center w-7/10">
         <motion.div
           initial={{ opacity: 0, x: -100 }}
           animate={{ opacity: 1, x: 0 }}
