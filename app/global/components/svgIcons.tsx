7d377e5db17f3ab5dff0667d23e7297285f5134b
Bot-on-Tapwater <mundabrandon@outlook.com>
2026-05-26 01:05:52 +0300

    chore: Add plus icon

--- app/global/components/svgIcons.tsx
+++ app/global/components/svgIcons.tsx
@@ -248,6 +248,24 @@ export function YoutubeIcon() {
   );
 }
 
+export function PlusIcon() {
+  return (
+    <>
+      <div className="">
+        <svg
+          fill="currentColor"
+          width="24px"
+          height="24px"
+          viewBox="0 0 32 32"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M9,17h6v6a1,1,0,0,0,2,0V17h6a1,1,0,0,0,0-2H17V9a1,1,0,0,0-2,0v6H9a1,1,0,0,0,0,2Z" />
+        </svg>
+      </div>
+    </>
+  );
+}
+
 export function InstagramIcon() {
   return (
     <>
